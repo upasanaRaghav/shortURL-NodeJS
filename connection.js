647183bc36e03eddcf58df8041f85325c6afd8ff
UpasanaRaghav <raghavupasana44@gmail.com>
2024-03-13 22:31:41 +0530

    Initial commit

--- connection.js
+++
@@ -1,9 +0,0 @@
-const mongoose=require('mongoose')
-mongoose.set("StrictQuery",true);
-async function connectToMongoAtlas(url){
-    return mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, session: options && options.session });
-}
-
-module.exports={
-    connectToMongoAtlas,
-};
